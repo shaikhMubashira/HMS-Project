271bbd70574757bfc5a685f89d6894d3f115c809
shaikhMubashira <shaikhmubashira9819@gmail.com>
2026-09-05 14:20:22 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Hotel REST INN</title>
+        <title>Hotel MTF INN</title>
         
         <!-- Core Layout Engine Framework -->
         <link rel="stylesheet" href="css/bootstrap.css">
